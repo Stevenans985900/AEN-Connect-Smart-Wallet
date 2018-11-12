0189eedfe81176095fbd8bdca5c1880280a4e466
Simon <simon@ionic-web.com>
2018-11-12 11:15:29 +0800

    commit missed test file

--- app/test/index.test.js
+++ app/test/index.test.js
@@ -1,5 +1,6 @@
 import test from 'ava'
 import { Nuxt, Builder } from 'nuxt'
+import { JSDOM } from 'jsdom'
 import { resolve } from 'path'
 
 // We keep a reference to Nuxt so we can close
@@ -7,34 +8,26 @@ import { resolve } from 'path'
 let nuxt = null
 
 // Init Nuxt.js and start listening on localhost:4000
-test.before('Init Nuxt.js', async t => {
-  const rootDir = resolve(__dirname, '..')
-  let config = {}
-  try { config = require(resolve(rootDir, 'nuxt.config.js')) } catch (e) {}
-  config.rootDir = rootDir // project folder
-  config.dev = false // production build
+test.before('Init Nuxt.js', async () => {
+	const rootDir = resolve(__dirname, '..')
+	let config = {}
+	try { config = require(resolve(rootDir, 'nuxt.config.js')) } catch (e) {}
+  config.rootDir = rootDir
+	config.dev = false
   config.mode = 'spa'
+
   nuxt = new Nuxt(config)
   await new Builder(nuxt).build()
-  nuxt.listen(4000, 'localhost')
-})
+  await nuxt.listen(4000, 'localhost')
+}, 30000)
 
 // Example of testing only generated html
 test('Route / exits and render HTML', async t => {
-  let context = {}
-  const { html } = await nuxt.renderRoute('/', context)
-  t.true(html.includes('Aenco Solutions Ltd - Global Health Blockchain Financial Solutions'))
+	const window = await nuxt.renderAndGetWindow('http://localhost:4000/')
+  const element = window.document.querySelector('.headline')
+	t.not(element, null)
+  t.is(element.textContent, 'Welcome to the AENChain Wallet (local)!')
 })
-
-// Example of testing via DOM checking
-// test('Route / exits and render HTML with CSS applied', async t => {
-//   const window = await nuxt.renderAndGetWindow('http://localhost:4000/')
-//   const element = window.document.querySelector('.red')
-//   t.not(element, null)
-//   t.is(element.textContent, 'Hello world!')
-//   t.is(element.className, 'red')
-//   t.is(window.getComputedStyle(element).color, 'red')
-// })
 
 // Close the Nuxt server
 test.after('Closing server', t => {
